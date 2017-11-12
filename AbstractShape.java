d5ddae819cf2bd3cd84f353f6dadcce661f81446
udomugo <jmorren.contact@gmail.com>
2017-11-11 22:02:16 -0800

    Added AbstractShape Methods
     * Added drawShape() method required by Shape Interface
     * Added toString() method implementation required by Shape Interface
     * Added addLevel() method implementation required by Shaper Interface
     Added removeLevel() method implementation required by Shape Interface
     * Added hasChildren() method implementation required by Shape Interface
     * Added createChildren() method implementation required by Shape
    Interface

--- AbstractShape.java
+++ AbstractShape.java
@@ -1,10 +1,12 @@
 import java.awt.Color;
+import java.awt.Graphics;
 
-public class AbstractShape {
+public class AbstractShape implements Shape{
 	
 	// fields of basic shape objects
 	protected Color c;
 	protected int x,y;
+	protected Shape[] childrenShapes;
 	
 	/**
 	 * AbstractShape Constructor
@@ -18,4 +20,53 @@ public class AbstractShape {
 		this.y = y;
 		this.c = c;
 	}
+
+	@Override
+	public void drawShape(Graphics g, Color c) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public String toString() {
+		return "" + this.getClass();
+	}
+	
+	public boolean addLevel() {
+		if (!this.hasChildren()) {
+			this.createChildren();
+		} else {
+			for (Shape s : childrenShapes) {
+				if (s.hasChildren()) //{
+					s.addLevel();
+//				} else {
+//					this.createChildren();
+//				}
+			}
+		}
+		return this.hasChildren();
+	}
+	
+	public boolean removeLevel() {
+		if (this.hasChildren()) {
+			for (Shape s : childrenShapes) {
+				s.removeLevel();
+			}
+		}
+		return this.hasChildren();
+	}
+	
+	public boolean hasChildren() {
+		boolean children = true;
+		for (Shape s : childrenShapes) {
+			if (s == null) {
+				children = false;
+			}
+		}
+		return children;
+	}
+	
+	public void createChildren() {
+		this.createChildren();
+	}
+	
 }
