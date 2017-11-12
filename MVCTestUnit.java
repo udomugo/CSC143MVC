85a165a836d21145411d3c98aa3a34a50f9853a6
udomugo <jmorren.contact@gmail.com>
2017-11-11 19:03:28 -0800

    Created New Tests in MVCTestUnit Class
     * Added testToString()
     	- Testing the toString() method in Shape Interface and implemented in
    the AbstractShape Class
     * Added testHasChildren()
     	- Testing the hasChildren() method in the Shape Interface and
    implemented in the AbstractShape Class
     * Added testCreateChildren()
     	- Testing the createChildren() method in the Shape Interface and
    implemented in the AbstractShape Class
     * Added testAddLevel()
     	- Testing the addLevel() method in the Shape Interface and implemented
    in the AbstractShape Class

--- MVCTestUnit.java
+++ MVCTestUnit.java
@@ -11,6 +11,11 @@ public class MVCTestUnit {
 //		fail("Not yet implemented");
 //	}
 	
+	/**
+	 * testGetFibonacciValue Method
+	 * Test checks that getFibonacciValue() in FibonacciSquare Class correctly
+	 * calculates the Fibonacci Number to the value of n 
+	 */
 	@Test
 	public void testGetFibonacciValue() {
 		FibonacciSquare testShape = new FibonacciSquare(600, 50, Color.orange,1,60);
@@ -21,6 +26,66 @@ public class MVCTestUnit {
 		int testResult2 = testShape.getFibonacciValue(20);
 		System.out.println(testResult2);
 		assertTrue(testResult2 == 6765);
+	}
+	
+	@Test
+	public void testToString() {
+		FibonacciSquare testShape1 = new FibonacciSquare(600, 50, Color.orange,1,60);
+		System.out.println(testShape1);
+		assertTrue(testShape1.toString().equals("class FibonacciSquare"));
+		
+		HShape testShape2 = new HShape(50,50,Color.green,90);
+		System.out.println(testShape2);
+		assertTrue(testShape2.toString().equals("class HShape"));
+	}
+	
+	@Test
+	public void testHasChildren() {
+		FibonacciSquare testShape1 = new FibonacciSquare(600, 50, Color.orange,1,60);
+		//System.out.println(testShape1);
+		assertTrue(!testShape1.hasChildren());
+		
+		HShape testShape2 = new HShape(50,50,Color.green,90);
+		//System.out.println(testShape2);
+		assertTrue(!testShape2.hasChildren());
+	}
+	
+	@Test
+	public void testCreateChildren() {
+		FibonacciSquare testShape1 = new FibonacciSquare(600, 50, Color.orange,1,60);
+		//System.out.println(testShape1);
+		assertTrue(!testShape1.hasChildren());
+		
+		HShape testShape2 = new HShape(50,50,Color.green,90);
+		//System.out.println(testShape2);
+		assertTrue(!testShape2.hasChildren());
+		
+		testShape1.createChildren();
+		testShape2.createChildren();
+		
+
+		assertTrue(testShape1.hasChildren());
+		assertTrue(testShape2.hasChildren());
+	}
+	
+	@Test
+	public void testAddLevel() {
+		FibonacciSquare testShape1 = new FibonacciSquare(600, 50, Color.orange,1,60);
+		//System.out.println(testShape1);
+		assertTrue(!testShape1.hasChildren());
+		
+		HShape testShape2 = new HShape(50,50,Color.green,90);
+		//System.out.println(testShape2);
+		assertTrue(!testShape2.hasChildren());
+		
+		assertTrue(testShape1.addLevel());
+		assertTrue(testShape1.hasChildren());
+		
+		assertTrue(testShape2.addLevel());
+		assertTrue(testShape2.hasChildren());
+
+		//assertTrue(testShape1.hasChildren());
+		//assertTrue(testShape2.hasChildren());
 	}
 
 }
