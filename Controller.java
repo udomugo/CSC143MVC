e9b70b89ea36e2535c35f68dcafdd314ee3e861b
udomugo <jmorren.contact@gmail.com>
2017-11-17 00:28:00 -0800

    Added radio button group
     * Radio buttons set state of mouseClick() event listener to call
    addLevel() or removeLevel() methods
     * Removed JButtons calling addLevel() and removeLeve() methods

--- Controller.java
+++ Controller.java
@@ -5,10 +5,13 @@ import java.awt.event.ComponentListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.util.ArrayList;
+import java.util.List;
 
 public class Controller implements ActionListener, ComponentListener, MouseListener{
 	
 	private DrawingModel model;
+	
+	private boolean level = true;
 	
 	private ArrayList<Viewer> viewers = new ArrayList<Viewer>();
 	
@@ -43,26 +46,47 @@ public class Controller implements ActionListener, ComponentListener, MouseListe
 
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
-		//System.out.println(arg0.getActionCommand());
+		String action = arg0.getActionCommand();
+		//System.out.println(action);
+		
 //		if (arg0.getActionCommand().equals("Add Level")) {
 //			model.addLevel();
 //			System.out.println("Add Button Pushed");
 //		} else 
-			if (arg0.getActionCommand().equals("Remove Level")) {
-			model.removeLevel();
-			System.out.println("Remove Button Pushed");
-		} else if (arg0.getActionCommand().equals("Reset Shapes")) {
+//			if (action.equals("Remove Level")) {
+//			model.removeLevel();
+//			System.out.println("Remove Button Pushed");
+//		} else
+		
+		if (action.equals("Reset Shapes")) {
 			model.resetShapes();
-			System.out.println("Reset Button Pushed");
+			//System.out.println("Reset Button Pushed");
+		}else if (action.equals("RadioAddLevel")) {
+			level = true;
+			//System.out.println("Radio Add Level");
+		} else if (action.equals("RadioRemoveLevel")) {
+			level = false;
+			//System.out.println("Radio Remove Level");
 		}
-		
-		
 	}
 
 	@Override
 	public void mouseClicked(MouseEvent arg0) {
 		// TODO Auto-generated method stub
-		
+		System.out.println("Click: ( " + (arg0.getX() - 10) + ", " + (arg0.getY() - 38) + " )");
+		List<Shape> shapes = model.getShapes(); 
+		for (int i = 0; i < shapes.size(); i++) {
+			if(shapes.get(i).checkClick(arg0.getX()-10, arg0.getY()-38)) {
+				if (this.level) {
+					model.addLevel(shapes.get(i));
+				} else {
+					model.removeLevel(shapes.get(i));
+				}
+				
+			} else {
+				System.out.println("Click not on Shape");
+			}	
+		}
 		
 	}
 
@@ -87,15 +111,7 @@ public class Controller implements ActionListener, ComponentListener, MouseListe
 	@Override
 	public void mouseReleased(MouseEvent arg0) {
 		// TODO Auto-generated method stub
-		System.out.println("Click: ( " + arg0.getX() + ", " + arg0.getY() + " )");
-		ArrayList<Shape> shapes = model.getShapes(); 
-		for (int i = 0; i < shapes.size(); i++) {
-			if(shapes.get(i).checkClick(arg0.getX(), arg0.getY())) {
-				model.addLevel(shapes.get(i));
-			} else {
-				System.out.println("Click not on Shape");
-			}	
-		}
+		
 	}
 	
 	
